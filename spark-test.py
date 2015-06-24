4b801764d37085e3e3d2bb714022efe39eed312c
Xiao (Shawn) Meng <meng14fall@gmail.com>
2015-06-24 16:30:54 -0400

    Designing queries.

--- spark-test.py
+++ spark-test.py
@@ -18,9 +18,10 @@ class tests():
 	def reg1(self):
 		rdd.filter(lambda line: re.search('facebook.[a-zA-Z0-9]|plus.google.[a-zA-Z0-9]|twitter.[a-zA-Z0-9]|[a-zA-Z0-9].qzone.qq.com|habbo.[a-zA-Z0-9]|vk.[a-zA-Z0-9]|linkedin.[a-zA-Z0-9]|instagram.[a-zA-Z0-9]|tagged.[a-zA-Z0-9]|gmail.[a-zA-Z0-9]|box.[a-zA-Z0-9]|apple.[a-zA-Z0-9]|dropbox.[a-zA-Z0-9]|google.[a-zA-Z0-9]|battlenet.[a-zA-Z0-9]|pinterest.[a-zA-Z0-9]|drive.google.[a-zA-Z0-9]',line)).count()
 
-	# reg query 2: match against certain actions
-		def reg2(self):
-		rdd.filter(lambda line: re.search('facebook \s disconnect|facebook \s not found|facebook \s slow|facebook \s redirect',line)).count()
+	# reg query 2: match against certain actions for facebook
+	def reg2(self):
+		rdd.filter(lambda line: re.search('facebook\sdisconnect|facebook\snotfound|facebook\sslow|facebook\sredirect',line)).count()
 
-	
-	
+	# reg query 3: match against certain actions for google
+	def reg3(self):
+	    rdd.filter(lambda line: re.search('google\sdisconnect|google\snotfound|google\sslow|google\sredirect', line)).count()
