e42f1405e80aae3e10b9053cbba80492d5a5ae9f
Xiao <issjtu2010@hotmail.com>
2015-06-24 00:52:57 -0400

    Designing reg exp queries.

--- spark-test.py
+++ spark-test.py
@@ -15,8 +15,12 @@ class tests():
 		rdd.filter(lambda line: "google" in line or "bing" in line).count()
 		
 	# reg query 1: match against major websites	
-	rdd.filter(lambda line: re.search('dropbox.[a-zA-Z0-9]|linkedin.[a-zA-Z0-9]',line)).count()
-	
-	rdd.map(lambda line: re.search("facebook.[a-zA-Z0-9]|plus.google.[a-zA-Z0-9]|twitter.[a-zA-Z0-9]|[a-z]{2,3}.qzone.qq.com|habbo.[a-z]{2,3}|vk.[a-z]{2,3}|linkedin.[a-z]{2,3}|instagram.[a-z]{2,3}|tagged.[a-z]{2,3}|gmail.[a-z]{2,3}|box.[a-z]{2,3}|apple.[a-z]{2,3}|dropbox.[a-z]{2,3}|google.[a-z]{2,3}|www.battlenet.[a-z]{2,3}|www.pinterest.[a-z]{2,3}|drive.google.[a-z]{2,3}", line)..count()
+	def reg1(self):
+		rdd.filter(lambda line: re.search('facebook.[a-zA-Z0-9]|plus.google.[a-zA-Z0-9]|twitter.[a-zA-Z0-9]|[a-zA-Z0-9].qzone.qq.com|habbo.[a-zA-Z0-9]|vk.[a-zA-Z0-9]|linkedin.[a-zA-Z0-9]|instagram.[a-zA-Z0-9]|tagged.[a-zA-Z0-9]|gmail.[a-zA-Z0-9]|box.[a-zA-Z0-9]|apple.[a-zA-Z0-9]|dropbox.[a-zA-Z0-9]|google.[a-zA-Z0-9]|battlenet.[a-zA-Z0-9]|pinterest.[a-zA-Z0-9]|drive.google.[a-zA-Z0-9]',line)).count()
+
+	# reg query 2: match against certain actions
+		def reg2(self):
+		rdd.filter(lambda line: re.search('facebook \s disconnect|facebook \s not found|facebook \s slow|facebook \s redirect',line)).count()
+
 	
 	
